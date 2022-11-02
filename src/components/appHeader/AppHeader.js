57ddc066ef8ec437e4705472146be5452eb27479
Myou5uf <myxamed12l@gmail.com>
2022-11-02 12:16:04 +0700

    fix link for CharactersPage

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,11 +1,11 @@
-import './appHeader.scss';
-import {Link, NavLink} from "react-router-dom";
+import "./appHeader.scss";
+import { Link, NavLink } from "react-router-dom";
 
 const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <Link to="/">
+                <Link to="/marvel">
                     <span>Marvel</span> information portal
                 </Link>
             </h1>
@@ -13,23 +13,22 @@ const AppHeader = () => {
                 <ul>
                     <li>
                         <NavLink
-                            to="/" end
-                            style={({isActive}) => isActive ? {color: "#9f0013"} : undefined}
-                        >Characters
+                            to="/marvel"
+                            end
+                            style={({ isActive }) => (isActive ? { color: "#9f0013" } : undefined)}>
+                            Characters
                         </NavLink>
                     </li>
                     /
                     <li>
-                        <NavLink
-                            to="/comics"
-                            style={({isActive}) => isActive ? {color: "#9f0013"} : undefined}
-                        >Comics
+                        <NavLink to="/comics" style={({ isActive }) => (isActive ? { color: "#9f0013" } : undefined)}>
+                            Comics
                         </NavLink>
                     </li>
                 </ul>
             </nav>
         </header>
-    )
-}
+    );
+};
 
 export default AppHeader;
